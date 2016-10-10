d00fc943fd52040f6c29fff72c16a633df67725a
tkrop <tronje.krop@zalando.de>
2016-10-10 11:35:43 +0200

    fixed bugs for release.

--- src/main/java/org/zalando/zjsonpatch/FeatureFlags.java
+++ src/main/java/org/zalando/zjsonpatch/FeatureFlags.java
@@ -10,15 +10,15 @@ import java.util.Set;
 public enum FeatureFlags {
 
     /**
-     * Enable general patch optimization feature after creating a patch via {@link JsonDiff#create(JsonNode, JsonNode)}
-     * or {@link JsonDiff#create(JsonNode, JsonNode, Set)}.
+     * Enable general patch optimization feature after creating a patch via {@link JsonPatch#create(JsonNode, JsonNode)}
+     * or {@link JsonPatch#create(JsonNode, JsonNode, Set)}.
      */
     PATCH_OPTIMIZATION,
 
     /**
      * Enable representation of nulls as missing values and vice versa, when creating or applying patches via
-     * {@link JsonDiff#create(JsonNode, JsonNode)}, {@link JsonDiff#create(JsonNode, JsonNode, Set)},
-     * {@link JsonPatch#apply(JsonNode, JsonNode)}, or {@link JsonPatch#apply(JsonNode, JsonNode, Set))}.
+     * {@link JsonPatch#create(JsonNode, JsonNode)}, {@link JsonPatch#create(JsonNode, JsonNode, Set)},
+     * {@link JsonPatch#apply(JsonNode, JsonNode)}, or {@link JsonPatch#apply(JsonNode, JsonNode, Set)}.
      */
     MISSING_VALUES_AS_NULLS,
 
